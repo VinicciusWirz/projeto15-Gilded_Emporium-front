0b47b1477c204605ec672faa5a2401724b7c7b16
Viniccius Wirz <vinimovel23@gmail.com>
2023-04-27 17:06:23 -0300

    Fix: organization / routers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyle from './assets/css/GlobalStyle';
+import ResetStyle from './assets/css/ResetStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ResetStyle/>
+    {/* <GlobalStyle/> */}
     <App />
   </React.StrictMode>
 );
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function HomePage() {
+  return <PageContainer>aaaaa</PageContainer>;
+}
+
+const PageContainer = styled.div`
+    /* margin-top: 10%; */
+  /* background: red; */
+`;
